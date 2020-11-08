f1a177364cc63c11d7527b9959fec822b8e6bf3e
Javier <jiruiz2@uc.cl>
2020-11-07 21:38:32 -0300

    :fire: deleted import_db.sh

--- import_db.sh
+++ import_db.sh
@@ -1,3 +1,3 @@
-echo "wena mi genteee"
-docker cp 01-products.json mongo:/tmp/01-products.json
-docker exec mongo mongoimport -d walmart_db -c walmart_collection --file /tmp/01-products.json
+# echo "wena mi genteee"
+# docker cp 01-products.json mongo:/tmp/01-products.json
+# docker exec mongo mongoimport -d walmart_db -c walmart_collection --file /tmp/01-products.json
